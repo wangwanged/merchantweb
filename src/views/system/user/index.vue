b8b7993ab128d7bf8e96abade7e7140454f88b3d
hanke <14703110113@163.com>
2020-10-22 15:54:23 +0800

    修改id

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -135,7 +135,7 @@
 
         <el-table v-loading="loading" :data="userList" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="50" align="center" />
-          <el-table-column label="用户编号" align="center" prop="userId" />
+          <el-table-column label="用户编号" align="center" prop="id" />
           <el-table-column label="用户名称" align="center" prop="userName" :show-overflow-tooltip="true" />
           <el-table-column label="用户昵称" align="center" prop="nickName" :show-overflow-tooltip="true" />
           <el-table-column label="部门" align="center" prop="dept.deptName" :show-overflow-tooltip="true" />
