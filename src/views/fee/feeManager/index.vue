e44f515ebb6e8bb7fcf4698910bddc5718c03318
pureshine <1309173081@qq.com>
2020-12-11 18:07:06 +0800

    费用管理新增
    Signed-off-by: pureshine <1309173081@qq.com>

--- src/views/fee/feeManager/index.vue
+++ src/views/fee/feeManager/index.vue
@@ -24,7 +24,7 @@
           size="small"
           @keyup.enter.native="handleQuery"
         />
-      </el-form-item> -->
+      </el-form-item>-->
       <el-form-item prop="shoukuanDate">
         <el-date-picker
           clearable
@@ -34,8 +34,7 @@
           type="date"
           value-format="yyyy-MM-dd"
           placeholder="选择收款时间"
-        >
-        </el-date-picker>
+        ></el-date-picker>
       </el-form-item>
       <el-form-item prop="type">
         <el-input
@@ -47,12 +46,7 @@
         />
       </el-form-item>
       <el-form-item prop="produce">
-        <el-select
-          v-model="queryParams.produce"
-          placeholder="请选择签约产品"
-          clearable
-          size="small"
-        >
+        <el-select v-model="queryParams.produce" placeholder="请选择签约产品" clearable size="small">
           <el-option
             v-for="dict in produceOptions"
             :key="dict.dictValue"
@@ -69,7 +63,7 @@
           size="small"
           @keyup.enter.native="handleQuery"
         />
-      </el-form-item> -->
+      </el-form-item>-->
       <!-- <el-form-item label="付款人" prop="payer">
         <el-input
           v-model="queryParams.payer"
@@ -87,14 +81,9 @@
           size="small"
           @keyup.enter.native="handleQuery"
         />
-      </el-form-item> -->
-      <el-form-item  prop="checkStatus">
-        <el-select
-          v-model="queryParams.checkStatus"
-          placeholder="请选择费用状态"
-          clearable
-          size="small"
-        >
+      </el-form-item>-->
+      <el-form-item prop="checkStatus">
+        <el-select v-model="queryParams.checkStatus" placeholder="请选择费用状态" clearable size="small">
           <el-option
             v-for="dict in checkStatusOptions"
             :key="dict.dictValue"
@@ -115,13 +104,7 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button
-          type="primary"
-          icon="el-icon-search"
-          size="mini"
-          @click="handleQuery"
-          >搜索</el-button
-        >
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
       </el-form-item>
     </el-form>
 
@@ -135,7 +118,7 @@
           v-hasPermi="['fee:feeManager:add']"
           >新增</el-button
         >
-      </el-col> -->
+      </el-col>-->
       <el-col :span="1.5">
         <el-button
           type="primary"
@@ -144,8 +127,7 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['fee:feeManager:edit']"
-          >修改</el-button
-        >
+        >修改</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -155,8 +137,7 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['fee:feeManager:remove']"
-          >删除</el-button
-        >
+        >删除</el-button>
       </el-col>
       <!-- <el-col :span="1.5">
         <el-button
@@ -167,7 +148,7 @@
           v-hasPermi="['fee:feeManager:export']"
           >导出</el-button
         >
-      </el-col> -->
+      </el-col>-->
       <div class="fr">
         <el-col :span="1.5">
           <el-button
@@ -175,44 +156,33 @@
             size="mini"
             @click="handleAdd"
             v-hasPermi="['fee:feeManager:add']"
-            >新增费用</el-button
-          >
+          >新增费用</el-button>
         </el-col>
-        
+
         <el-col :span="1.5">
           <el-button
             type="primary"
             size="mini"
             @click="handleExport"
             v-hasPermi="['fee:feeManager:export']"
-            >导出</el-button
-          >
+          >导出</el-button>
         </el-col>
       </div>
     </el-row>
 
-    <el-table
-      v-loading="loading"
-      :data="feeManagerList"
-      @selection-change="handleSelectionChange"
-    >
+    <el-table v-loading="loading" :data="feeManagerList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
       <!-- <el-table-column label="合同费用表id" align="center" prop="id" /> -->
-      <el-table-column label="公司" align="center" prop="sysCompany.name" />
+      <el-table-column label="公司" align="center" prop="companyName" />
       <el-table-column label="收费时间" align="center" prop="shoukuanDate">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.shoukuanDate, "{y}-{m}-{d}") }}</span>
         </template>
       </el-table-column>
       <el-table-column label="费用编号" align="center" prop="num" width="180" />
-      <el-table-column label="费用类型" align="center" prop="type" />
+      <el-table-column label="费用类型" align="center" prop="type" :formatter="feeTypeFormat"/>
       <el-table-column label="费用金额" align="center" prop="amount" />
-      <el-table-column
-        label="付款方式"
-        align="center"
-        prop="payMethod"
-        :formatter="payMethodFormat"
-      />
+      <el-table-column label="付款方式" align="center" prop="payMethod" :formatter="payMethodFormat" />
       <el-table-column label="收款人" align="center" prop="reciever" />
       <el-table-column
         label="费用状态"
@@ -220,38 +190,22 @@
         prop="checkStatus"
         :formatter="checkStatusFormat"
       />
-      <el-table-column
-        label="合同编号"
-        align="center"
-        prop="contractNum"
-        width="180"
-      >
+      <el-table-column label="合同编号" align="center" prop="contractNum" width="180">
         <template slot-scope="obj">
-          <el-button
+          <span
             @click="
               $router.push({
                 path: '/contract/contractItem',
                 query: { id: obj.row.id },
               })
             "
-            size="small"
-            type="text"
-            >{{ obj.row.contractNum }}</el-button
-          >
+            style="color: #1890FF;cursor: pointer;"
+          >{{ obj.row.contractNum }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="客户姓名" align="center" prop="payer" />
-      <el-table-column
-        label="签约产品"
-        align="center"
-        prop="produce"
-        :formatter="produceFormat"
-      />
-      <el-table-column
-        label="店面/区域名称"
-        align="center"
-        prop="dianmianName"
-      />
+      <el-table-column label="客户姓名" align="center" prop="customerName" />
+      <el-table-column label="签约产品" align="center" prop="produce" :formatter="produceFormat" />
+      <el-table-column label="店面/区域名称" align="center" prop="dianmianName" />
       <el-table-column label="负责人" align="center" prop="manager" />
       <!-- <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
@@ -270,7 +224,7 @@
             v-hasPermi="['fee:feeManager:remove']"
           >删除</el-button>
         </template>
-      </el-table-column> -->
+      </el-table-column>-->
     </el-table>
 
     <pagination
@@ -284,28 +238,51 @@
     <!-- 添加或修改费用管理对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="费用编号" prop="num">
-          <el-input v-model="form.num" placeholder="请输入费用编号" />
-        </el-form-item>
         <el-form-item label="合同编号" prop="contractNum">
-          <el-input v-model="form.contractNum" placeholder="请输入合同编号" />
+          <!-- <el-input v-model="form.contractNum" placeholder="请输入合同编号" /> -->
+          <el-select
+            v-model="form.contractId"
+            filterable
+            remote
+            reserve-keyword
+            placeholder="请输入合同编号"
+            :remote-method="remoteMethod"
+            :loading="loading"
+            @change="contractChange"
+          >
+            <el-option
+              v-for="item in contractList"
+              :key="item.contractId"
+              :label="item.contractNum + '('+item.customerName+')'"
+              :value="item.contractId"
+            >{{item.contractNum}}（{{item.customerName}}）</el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="收据编号" prop="num">
+          <el-input v-model="form.num" placeholder="请输入收据编号" />
         </el-form-item>
         <el-form-item label="费用类型" prop="type">
-          <el-input v-model="form.type" placeholder="请输入费用类型" />
+          <el-select v-model="form.type" placeholder="请选择费用类型">
+            <el-option
+              v-for="dict in feeTypeOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            ></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="付款方式" prop="payMethod">
-          <el-input v-model="form.payMethod" placeholder="请输入付款方式" />
-        </el-form-item>
-        <el-form-item label="付款人" prop="payer">
-          <el-input v-model="form.payer" placeholder="请输入付款人" />
+          <el-select v-model="form.payMethod" placeholder="请选择付款方式">
+            <el-option
+              v-for="dict in payMethodOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            ></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="收款人" prop="reciever">
           <el-input v-model="form.reciever" placeholder="请输入收款人" />
-        </el-form-item>
-        <el-form-item label="费用状态">
-          <el-radio-group v-model="form.checkStatus">
-            <el-radio label="1">请选择字典生成</el-radio>
-          </el-radio-group>
         </el-form-item>
         <el-form-item label="收款时间" prop="shoukuanDate">
           <el-date-picker
@@ -316,20 +293,7 @@
             type="date"
             value-format="yyyy-MM-dd"
             placeholder="选择收款时间"
-          >
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="更新时间" prop="updateDate">
-          <el-date-picker
-            clearable
-            size="small"
-            style="width: 200px"
-            v-model="form.updateDate"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="选择更新时间"
-          >
-          </el-date-picker>
+          ></el-date-picker>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -343,17 +307,22 @@
 <script>
 import {
   listFeeManager,
+  listContractManager,
   getFeeManager,
   delFeeManager,
   addFeeManager,
   updateFeeManager,
   exportFeeManager,
 } from "@/api/fee/feeManager";
+import { getUserProfile } from "@/api/system/user";
 
 export default {
   name: "FeeManager",
   data() {
     return {
+      user: {},
+      //合同编号数组
+      contractList:[],
       // 遮罩层
       loading: true,
       // 选中数组
@@ -379,6 +348,8 @@ export default {
       checkStatusOptions: [],
       // 签约产品字典
       produceOptions: [],
+      // 费用类型字典
+      feeTypeOptions: [],
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -398,32 +369,26 @@ export default {
       rules: {
         num: [{ required: true, message: "费用编号不能为空", trigger: "blur" }],
         contractNum: [
-          { required: true, message: "合同编号不能为空", trigger: "blur" },
+          { required: true, message: "合同编号不能为空", trigger: "change" },
         ],
         type: [
-          { required: true, message: "费用类型不能为空", trigger: "blur" },
+          { required: true, message: "费用类型不能为空", trigger: "change" },
         ],
         payMethod: [
-          { required: true, message: "付款方式不能为空", trigger: "blur" },
+          { required: true, message: "付款方式不能为空", trigger: "change" },
         ],
-        payer: [{ required: true, message: "付款人不能为空", trigger: "blur" }],
         reciever: [
           { required: true, message: "收款人不能为空", trigger: "blur" },
         ],
-        checkStatus: [
-          { required: true, message: "费用状态不能为空", trigger: "blur" },
-        ],
         shoukuanDate: [
           { required: true, message: "收款时间不能为空", trigger: "blur" },
-        ],
-        updateDate: [
-          { required: true, message: "更新时间不能为空", trigger: "blur" },
-        ],
+        ]
       },
     };
   },
   created() {
     this.getList();
+    this.getUser();
     this.getDicts("pay_method").then((response) => {
       this.payMethodOptions = response.data;
     });
@@ -433,8 +398,21 @@ export default {
     this.getDicts("sys_user_need").then((response) => {
       this.produceOptions = response.data;
     });
+    this.getDicts("fee_type").then((response) => {
+      this.feeTypeOptions = response.data;
+    });
   },
   methods: {
+    contractChange(item) {
+      this.$forceUpdate();
+      this.form.contractNum = this.contractList.find(res => res.contractId == item).contractNum
+    },
+    //获取当前登录用户信息
+    getUser() {
+      getUserProfile().then(response => {
+        this.user = response.data;
+      });
+    },
     /** 查询费用管理列表 */
     getList() {
       this.loading = true;
@@ -443,6 +421,22 @@ export default {
         this.total = response.total;
         this.loading = false;
       });
+    },
+    //获取合同编号列表
+    remoteMethod(query) {
+        if (query !== '') {
+        listContractManager({keyword:query}).then((response) => {
+          this.contractList = response.rows.map((item) => {
+              return{
+                ...item,
+                contractNum: item.num,
+                contractId: item.id
+              }
+          });
+        });
+        } else {
+          this.contractList = [];
+        }
     },
 
     // 付款方式字典翻译
@@ -457,6 +451,10 @@ export default {
     produceFormat(row, column) {
       return this.selectDictLabel(this.produceOptions, row.produce);
     },
+    // 费用类型字典翻译
+    feeTypeFormat(row, column) {
+      return this.selectDictLabel(this.feeTypeOptions, row.type);
+    },
     // 取消按钮
     cancel() {
       this.open = false;
@@ -470,11 +468,9 @@ export default {
         contractNum: null,
         type: null,
         payMethod: null,
-        payer: null,
         reciever: null,
-        checkStatus: "0",
         shoukuanDate: null,
-        updateDate: null,
+        contractId: null
       };
       this.resetForm("form");
     },
@@ -498,16 +494,20 @@ export default {
     handleAdd() {
       this.reset();
       this.open = true;
+      this.form.reciever = this.user.userName
       this.title = "新增费用";
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
       const id = row.id || this.ids;
+      this.remoteMethod(' ')
       getFeeManager(id).then((response) => {
         this.form = response.data;
         this.open = true;
         this.title = "编辑费用";
+        this.form.contractId = 24
+        console.log(this.form,999)
       });
     },
     /** 提交按钮 */
